fca0fe1fe691bcc8d41f6ff428b493727193cb74
System Administrator <root@Evangelias-MacBook-Air.local>
2019-05-30 18:34:19 +0300

    #3 code cleanup card grid list

--- src/components/organisms/CoursesCardList/CoursesCardList.jsx
+++ src/components/organisms/CoursesCardList/CoursesCardList.jsx
@@ -24,51 +24,6 @@ class CoursesCardList extends React.Component {
                              allergy={response.allery}
                              />
                 })}
-               {/* <Grid item xs={6} sm={4} md={3}>
-                    <CourseCard 
-                        image = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F200707-r-xl-grilled-vegetable-bruschetta.jpg%3Fitok%3DkP4cvjXP&w=1600&q=70"
-                        title ="Grilled Vegetable Bruschetta"
-                        description="What could be more summery than grilled peppers and squash heaped on toasted country bread that's slathered with fresh basil pesto?"
-                        allergy="milk, egg"
-                        spicy="1"
-                    />
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <CourseCard 
-                        image = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F200707-r-xl-grilled-vegetable-bruschetta.jpg%3Fitok%3DkP4cvjXP&w=1600&q=70"
-                        title ="Grilled Vegetable Bruschetta"
-                        description="What could be more summery than grilled peppers and squash heaped on toasted country bread that's slathered with fresh basil pesto?"
-                        allergy="milk, egg"
-                        spicy="1"
-                    />
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <CourseCard 
-                         image = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F200707-r-xl-grilled-vegetable-bruschetta.jpg%3Fitok%3DkP4cvjXP&w=1600&q=70"
-                        title ="Grilled Vegetable Bruschetta"
-                        description="What could be more summery than grilled peppers and squash heaped on toasted country bread that's slathered with fresh basil pesto?"
-                        allergy="milk, egg"
-                        spicy="1"
-                    />
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <CourseCard 
-                        image = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F200707-r-xl-grilled-vegetable-bruschetta.jpg%3Fitok%3DkP4cvjXP&w=1600&q=70"
-                        title ="Grilled Vegetable Bruschetta"
-                        description="What could be more summery than grilled peppers and squash heaped on toasted country bread that's slathered with fresh basil pesto?"
-                        allergy="milk, egg"
-                        spicy="1"
-                    />
-                </Grid>
-                <Grid item xs={6} sm={4} md={3}>
-                    <CourseCard 
-                        image = "https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2F200707-r-xl-grilled-vegetable-bruschetta.jpg%3Fitok%3DkP4cvjXP&w=1600&q=70"
-                        title ="Grilled Vegetable Bruschetta"
-                        description="What could be more summery than grilled peppers and squash heaped on toasted country bread that's slathered with fresh basil pesto?"
-                        allergy="milk, egg"
-                        spicy="1"
-                    />
-            </Grid>*/}
             </Grid>
         );
     }
