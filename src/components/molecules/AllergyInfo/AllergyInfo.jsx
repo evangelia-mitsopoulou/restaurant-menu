05bb6620ed8b61a70fcab7ec6121c240d012d942
Evangelia <evangelia.mitsopoulou@gmail.com>
2019-06-07 18:24:27 +0300

    #17 remove trailing comma from last allergic ingredient in results page

--- src/components/molecules/AllergyInfo/AllergyInfo.jsx
+++ src/components/molecules/AllergyInfo/AllergyInfo.jsx
@@ -2,19 +2,34 @@ import React from 'react';
 import './allergyInfo.scss';
 
 
-function AllergyInfo(props) {
+class AllergyInfo extends React.Component {
 
-    const list = Array.from(props.list);
+    constructor(props){
+        super(props);
+    }
 
-    return (
-        <div className='allergy-container'>
-            <span><i> Allergic Ingredients: </i></span>
-            {list.map((response) => {
-                return <span><i>{response}</i>, </span>
-            })}
-        </div>
-    );
+    componentWillMount(){
+        this.list = Array.from(this.props.list);
+    }
 
+
+    renderAllergyInfo= (item,f)=>{
+        return (<span><i>{item}</i>{f ? ',' : ' '} </span>)
+    }
+
+    render() {
+        return (
+            <div className='allergy-container'>
+                <span><i> Allergic Ingredients: </i></span>
+                {this.list.map((item,i) => {
+                    let flag = true;
+                    if (i === this.list.length -1) flag = false;
+                    return this.renderAllergyInfo(item,flag);
+                })}
+            </div>
+        );
+
+    }
 
 }
 
