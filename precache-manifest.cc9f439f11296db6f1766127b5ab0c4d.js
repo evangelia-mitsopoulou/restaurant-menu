397585b5e140a83f3d68cb50c441be6e52a22b2c
Evangelia <evangelia.mitsopoulou@gmail.com>
2019-06-08 17:04:12 +0300

    Updates

---
+++ precache-manifest.cc9f439f11296db6f1766127b5ab0c4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d0eb275dc04357b100e66e31a18769",
+    "url": "/restaurant-menu/index.html"
+  },
+  {
+    "revision": "685754a5147edb421274",
+    "url": "/restaurant-menu/static/css/main.71c576f0.chunk.css"
+  },
+  {
+    "revision": "94e4c383a55083249f7e",
+    "url": "/restaurant-menu/static/js/2.f942f0a1.chunk.js"
+  },
+  {
+    "revision": "685754a5147edb421274",
+    "url": "/restaurant-menu/static/js/main.f9ee2880.chunk.js"
+  },
+  {
+    "revision": "b20dd83e36889b5892dd",
+    "url": "/restaurant-menu/static/js/runtime~main.07df0635.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/restaurant-menu/precache-manifest.e026678eb5914219b446225f2417e422.js"
+  "/restaurant-menu/precache-manifest.cc9f439f11296db6f1766127b5ab0c4d.js"
 );
 
 self.addEventListener('message', (event) => {
