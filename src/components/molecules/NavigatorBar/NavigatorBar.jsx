74042e71db026ff11a97806f04d8f07945e92ae3
Evangelia <evangelia.mitsopoulou@gmail.com>
2019-06-08 10:21:15 +0300

    #18 navigator bar as css module

--- src/components/molecules/NavigatorBar/NavigatorBar.jsx
+++ src/components/molecules/NavigatorBar/NavigatorBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './navigatorBar.scss';
+import styles from './navigatorBar.module.scss';
 import Button from '@material-ui/core/Button';
 
 
@@ -7,7 +7,7 @@ class NavigatorBar extends React.Component {
 
     render(){
         return (
-            <div className='navigator'>
+            <div className={styles.navigator}>
                 <Button disabled={this.props.activeButton === false} variant="contained" color="primary" onClick={this.props.onClick}>
                     Next step
                  </Button>
